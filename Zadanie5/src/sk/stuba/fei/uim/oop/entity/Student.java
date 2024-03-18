85193003624cb82ac8a6b8cd83e13c885d512990
Dominik Šuráni <xsurani@stuba.sk>
2024-03-18 14:48:07 +0100

    Added package "entity" and classes "Course" & "Student"

--- Zadanie5/src/sk/stuba/fei/uim/oop/entity/Student.java
+++ Zadanie5/src/sk/stuba/fei/uim/oop/entity/Student.java
@@ -1,4 +1,33 @@
 package sk.stuba.fei.uim.oop.entity;
 
 public class Student {
+    public Student() { //konstruktor
+    }
+
+    private String name; //instancie
+    private int id;
+
+    public String getName() { //getter
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) { //setter
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "meno: " + this.name + " ID: " + this.id;
+    }
+    public static void registerToCourse(Course course){
+        registerStudent(course);
+    }
 }
