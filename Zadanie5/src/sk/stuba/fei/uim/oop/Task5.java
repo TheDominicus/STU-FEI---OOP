98cdefe11a904424a7f751a9b654f4c86d276c4a
Dominik Šuráni <127381529+TheDominicus@users.noreply.github.com>
2024-03-20 07:38:41 +0100

    Delete Zadanie5 directory

--- Zadanie5/src/sk/stuba/fei/uim/oop/Task5.java
+++
@@ -1,7 +0,0 @@
-package sk.stuba.fei.uim.oop;
-
-public class Task5 {
-    public static void main(String[] args) {
-        String[] names = new String[9];
-    }
-}
--- Zadanie5/src/sk/stuba/fei/uim/oop/Task5_2.java
+++
@@ -1,6 +0,0 @@
-package sk.stuba.fei.uim.oop;
-
-public class Task5_2 {
-    public static void main(String[] args) {
-    }
-}
